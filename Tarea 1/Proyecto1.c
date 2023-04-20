8019091fdcb4197a8fade7042e877c38453827ef
Diego Jimenez <diegojmoravia@gmail.com>
2023-04-19 19:59:34 -0600

    Random numbers generator and players chosing logic

--- Tarea 1/Proyecto1.c
+++ Tarea 1/Proyecto1.c
@@ -1,10 +1,87 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
 #include <time.h>
+#include <unistd.h>
+
+#define BET 10
+#define EARNIGS 360
+#define MIN_NUMBER_TO_GUESS 0
+#define MAX_NUMBER_TO_GUESS 36
 
 int bankBalance = 5000;
 int numberToGuess;
+
+// locker initialization
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+// generate a random integer
+int generarRandomInteger(int min, int max) {
+    srand(time(NULL)); 
+
+    int rango = max - min + 1; 
+    int numAleatorio = rand() % rango + min; 
+
+    return numAleatorio; 
+}
+
+
+void * updateNumber(){
+
+    while(1){    
+
+            pthread_mutex_lock(&mutex);
+            numberToGuess = generarRandomInteger(MIN_NUMBER_TO_GUESS,MAX_NUMBER_TO_GUESS);
+            printf("El nuevo numero es %d \n",numberToGuess);
+
+            pthread_mutex_unlock(&mutex);
+
+            sleep(3);
+    }
+
+}
+
+void *concrete(){
+
+    int playerBalance = 1000;
+    int guessedNumber;
+    char threadName[16];
+    pthread_t thread_id = pthread_self();
+    pthread_getname_np(thread_id, threadName, 16);
+
+    while(1){
+        
+        pthread_mutex_lock(&mutex);
+        guessedNumber = generarRandomInteger(MIN_NUMBER_TO_GUESS+1,MAX_NUMBER_TO_GUESS);
+        printf("el thread %s eligio %d \n",threadName,guessedNumber);
+        pthread_mutex_unlock(&mutex);
+        sleep(2);
+    }
+
+
+}
+
+void playConcreteNumber(){
+    pthread_t players[4];
+    pthread_t cropier;
+
+    pthread_create(&cropier, NULL, updateNumber, NULL);
+
+    for (int i = 0; i < 4; i++) {
+        pthread_create(&(players[i]), NULL, concrete, NULL);
+        char thread_name[17];
+        sprintf(thread_name, "player->%d", i+1);
+        pthread_setname_np(players[i], thread_name);
+    }
+
+    pthread_join(cropier, NULL);
+    
+    for (int i = 0; i < 4; i++) {
+        pthread_join(players[i], NULL);
+    }
+}
+
 
 int pickOption(){
    
@@ -18,41 +95,11 @@ int pickOption(){
 }
 
 
-// locker initialization
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 int main(){
     
-   srand(time(NULL));
-   
-   // initialazing number to guess between 1 and 36
-   
-   numberToGuess = rand() % (36- 1 + 1) + 1;
-  
-  // array that contains the 4 players
-   pthread_t players[4];
-   
-   
-   int opc = pickOption();
-   
-   switch(opc){
-       
-       case 1:
-            printf("Eligio al azar");
-            break;
-            case 2:
-                printf("Eligio numero par");
-                break;
-                case 3:
-                    printf("Eligio martingala");
-                    break;
-                    
-       
-       
-   }
-   
-   
-   
+    playConcreteNumber();
+
 
     return 0;
 }
